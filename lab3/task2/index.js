d4ceef9b92b1d9d09fb470dbba42b8d5bac3dcc6
Karakat <156986425+kakostur@users.noreply.github.com>
2025-02-06 01:27:17 +0500

    Add files via upload

---
+++ lab3/task2/index.js
@@ -0,0 +1,39 @@
+let listElement = document.getElementById("list-of-tasks")
+let inputLine = document.getElementById("myInput")
+let addButton = document.querySelector("button")
+let itemArrayList = []
+
+function addNewItem(thisInputValue){
+    let newItemElement = `<div class = "task-item">
+    <div class="item">       
+        <p class = "title"> <input type="checkbox" class="checkbox" /> ${thisInputValue} </p>
+        <button class="trash-button">
+            <img src="trash.png" alt="Trash icon">
+        </button>
+        </div>
+        <br>
+    </div>`
+    itemArrayList.push(thisInputValue)
+    listElement.innerHTML += newItemElement;
+    inputLine.value = ""
+}
+
+listElement.addEventListener('click', (element) => {
+    if(element.target.classList.contains("trash-button"))
+    element.target.parentElement.remove()
+})
+
+listElement.addEventListener('click', (element) => {
+    if(element.target.classList.contains("checkbox")){
+        if(element.target.parentElement.classList.contains("completed"))
+            element.target.parentElement.classList.remove("completed")
+        else
+            element.target.parentElement.classList.add("completed")
+    }
+})
+
+
+addButton.addEventListener('click', () =>{
+    inputValue = inputLine.value
+    addNewItem(inputValue)
+})
