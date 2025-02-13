ff885f27c56fb44f73b4599771e4ad366703cb0a
Karakat <156986425+kakostur@users.noreply.github.com>
2025-02-13 12:19:35 +0500

    Update app.config.ts

--- lab4/src/app/app.config.ts
+++ lab4/src/app/app.config.ts
@@ -2,5 +2,5 @@ import { provideRouter } from '@angular/router';
 import { ApplicationConfig } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter([])] // Просто оставляем роутер
+  providers: [provideRouter([])] 
 };
